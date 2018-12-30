b81ee44e920bf9e5f0a4fe010d706f31522c10fd
Amit Saxena <amit81saxena@gmail.com>
2018-12-30 11:30:43 -0800

    new test case added

--- test_example.py
+++ test_example.py
@@ -1,4 +1,7 @@
 import src.example1 as ex
 
-def test_add_munbers():
+def test_add_munbers_pass():
 	assert ex.add_numbers(10,20) == 30
+	
+def test_add_munbers_fail():
+	assert ex.add_numbers(10,20) == 50
